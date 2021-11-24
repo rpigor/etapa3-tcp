58dcbccb4b9441d0c4a9c1442411f032df2eaa54
Igor R <88985765+rpigor@users.noreply.github.com>
2021-11-23 22:23:09 -0300

    Adiciona mais testes unitários para a classe Evaluator

--- src/app/business/domain/EvaluatorTest.java
+++ src/app/business/domain/EvaluatorTest.java
@@ -10,9 +10,10 @@ import org.junit.Test;
 
 public class EvaluatorTest {
 
-    private Evaluator evaluator1, evaluator2, evaluator3;
+    private Evaluator evaluator1, evaluator2, evaluator3, evaluator4;
     private Product testProduct1, testProduct2, testProduct3;
     private EvaluationGroup eg1, eg2;
+    private Evaluation ev1, ev2, ev3, ev4, ev5, ev6, ev7, ev8;
 
     @Before
     public void setUp() throws Exception {
@@ -22,6 +23,8 @@ public class EvaluatorTest {
                 new LinkedList<Category>(Arrays.asList(Category.DD_CREAM, Category.POWDER_SUNSCREEN)));
         evaluator3 = new Evaluator(3, "Joe", "SP", 
                 new LinkedList<Category>(Arrays.asList(Category.BB_CREAM)));
+        evaluator4 = new Evaluator(4, "Jane", "RS", 
+                new LinkedList<Category>(Arrays.asList()));
         
         eg1 = new EvaluationGroup(null, new LinkedList<Evaluator>(Arrays.asList(evaluator1, evaluator2)));
         eg2 = new EvaluationGroup(null, new LinkedList<Evaluator>(Arrays.asList(evaluator1, evaluator2)));
@@ -30,17 +33,68 @@ public class EvaluatorTest {
         testProduct2 = new Product(2, null, evaluator1, eg1, Category.OIL_FREE_MATTE_SPF);
         testProduct3 = new Product(3, null, evaluator3, eg2, Category.BB_CREAM);
         
-        evaluator1.allowProduct(testProduct1);
-        evaluator1.allowProduct(testProduct2).rate(3);
+        ev1 = evaluator1.allowProduct(testProduct1);
+        ev2 = evaluator1.allowProduct(testProduct2);
+        ev2.rate(3);
         
-        evaluator2.allowProduct(testProduct1).rate(-1);
-        evaluator2.allowProduct(testProduct2).rate(-2);
-        evaluator2.allowProduct(testProduct3).rate(0);
+        ev3 = evaluator2.allowProduct(testProduct1);
+        ev3.rate(-1);
+        ev4 = evaluator2.allowProduct(testProduct2);
+        ev4.rate(-2);
+        ev5 = evaluator2.allowProduct(testProduct3);
+        ev5.rate(0);
         
-        evaluator3.allowProduct(testProduct1).rate(3);
-        evaluator3.allowProduct(testProduct2).rate(0);
-        evaluator3.allowProduct(testProduct3);
+        ev6 = evaluator3.allowProduct(testProduct1);
+        ev6.rate(3);
+        ev7 = evaluator3.allowProduct(testProduct2);
+        ev7.rate(0);
+        ev8 = evaluator3.allowProduct(testProduct3);
     }
+    
+	@Test
+	public void testGetId() {
+		assertTrue(evaluator1.getId() == 1);
+		assertFalse(evaluator2.getId() != 2);
+		assertFalse(evaluator1.getId() == 0);
+	}
+	@Test
+	public void testGetName() {
+		assertTrue(evaluator1.getName().equals("Mike"));
+		assertTrue(evaluator2.getName().equals("Nicolas"));
+		assertFalse(evaluator1.getName().equals("Nick"));
+	}
+
+	@Test
+	public void testGetState() {
+		assertTrue(evaluator1.getState().equals("SP"));
+		assertTrue(evaluator2.getState().equals("RS"));
+		assertFalse(evaluator1.getState().equals(""));
+	}
+
+	@Test
+	public void testGetInterestCategories() {
+		assertTrue(evaluator1.getInterestCategories().equals(Arrays.asList()));
+		assertFalse(evaluator1.getInterestCategories().equals(Arrays.asList(Category.DD_CREAM, Category.POWDER_SUNSCREEN)));
+		assertTrue(evaluator2.getInterestCategories().equals(Arrays.asList(Category.DD_CREAM, Category.POWDER_SUNSCREEN)));
+		assertTrue(evaluator3.getInterestCategories().equals(Arrays.asList(Category.BB_CREAM)));
+		assertFalse(evaluator3.getInterestCategories().equals(Arrays.asList()));
+	}
+
+	@Test
+	public void testGetEvaluations() {
+		assertTrue(evaluator1.getEvaluations().equals(Arrays.asList(ev1, ev2)));
+		assertFalse(evaluator1.getEvaluations().equals(Arrays.asList()));
+		assertTrue(evaluator2.getEvaluations().equals(Arrays.asList(ev3, ev4, ev5)));
+		assertTrue(evaluator3.getEvaluations().equals(Arrays.asList(ev6, ev7, ev8)));
+		assertTrue(evaluator4.getEvaluations().equals(Arrays.asList()));
+		assertFalse(evaluator4.getEvaluations().equals(Arrays.asList(ev1)));
+	}
+	
+	@Test
+	public void testAllowProduct() {
+		assertTrue(evaluator1.allowProduct(testProduct1) == null);
+		assertFalse(evaluator4.allowProduct(testProduct1) == null);
+	}
     
     @Test
    	public void testIsProductCandidate() {
