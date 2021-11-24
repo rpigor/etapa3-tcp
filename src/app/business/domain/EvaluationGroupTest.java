740e639679fc1dada66d7c4d9d563c8ef638496e
Igor R <88985765+rpigor@users.noreply.github.com>
2021-11-23 22:57:23 -0300

    Adiciona mais testes unitários para a classe EvaluationGroup

--- src/app/business/domain/EvaluationGroupTest.java
+++ src/app/business/domain/EvaluationGroupTest.java
@@ -2,15 +2,18 @@ package app.business.domain;
 
 import static org.junit.Assert.*;
 
+import java.util.AbstractMap;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class EvaluationGroupTest {
 	
-	private Evaluator evaluator1, evaluator2, evaluator3;
+	private Evaluator evaluator1, evaluator2, evaluator3, evaluator4;
 	private EvaluationGroup eg1, eg2, eg3, eg4;
 	private Product testProduct1, testProduct2, testProduct3, testProduct4, testProduct5, testProduct6;
 	
@@ -20,8 +23,9 @@ public class EvaluationGroupTest {
 		evaluator1 = new Evaluator(10, null, null, null);
 		evaluator2 = new Evaluator(9, null, null, null);
 		evaluator3 = new Evaluator(8, null, null, null);
+		evaluator4 = new Evaluator(7, null, null, null);
 		
-		eg1 = new EvaluationGroup("eg1", new LinkedList<Evaluator>(Arrays.asList(evaluator1, evaluator2, evaluator3)));
+		eg1 = new EvaluationGroup("eg1", new LinkedList<Evaluator>(Arrays.asList(evaluator1, evaluator2, evaluator3, evaluator4)));
 		eg2 = new EvaluationGroup("eg2", new LinkedList<Evaluator>(Arrays.asList(evaluator1, evaluator3)));
 		eg3 = new EvaluationGroup("eg3", new LinkedList<Evaluator>(Arrays.asList(evaluator2, evaluator3)));
 		eg4 = new EvaluationGroup("eg4", new LinkedList<Evaluator>(Arrays.asList(evaluator2)));
@@ -45,6 +49,9 @@ public class EvaluationGroupTest {
 		
 		evaluator3.allowProduct(testProduct2);
 		evaluator3.allowProduct(testProduct5);
+		
+		evaluator4.allowProduct(testProduct1).rate(-3);
+		evaluator4.allowProduct(testProduct2).rate(1);
 	}
 
 	@Test
@@ -57,7 +64,7 @@ public class EvaluationGroupTest {
 	
 	@Test
 	public void testGetMembers() {
-		assertTrue(eg1.getMembers().equals(Arrays.asList(evaluator1, evaluator2, evaluator3)));
+		assertTrue(eg1.getMembers().equals(Arrays.asList(evaluator1, evaluator2, evaluator3, evaluator4)));
 		assertFalse(eg2.getMembers().equals(Arrays.asList(evaluator1)));
 		assertTrue(eg3.getMembers().equals(Arrays.asList(evaluator2, evaluator3)));
 		assertFalse(eg3.getMembers().equals(Arrays.asList(evaluator3, evaluator2)));
@@ -92,6 +99,21 @@ public class EvaluationGroupTest {
 		assertTrue(eg2.hasPendingEvaluation());
 		assertFalse(eg3.hasPendingEvaluation());
 		assertFalse(eg4.hasPendingEvaluation());
+	}
+	
+	@Test
+	public void testGetAcceptableAndUnacceptableProductsMean() {
+		Map<Product, Double> acceptableProductsMeanEG1 = new HashMap<Product, Double>();
+		acceptableProductsMeanEG1.put(testProduct2, Double.valueOf(1.5));
+		acceptableProductsMeanEG1.put(testProduct3, Double.valueOf(1));
+		Map<Product, Double> unacceptableProductsMeanEG1 = new HashMap<Product, Double>();
+		unacceptableProductsMeanEG1.put(testProduct1, Double.valueOf(-1.5));	
+		assertTrue(eg1.getAcceptableAndUnacceptableProductsMean(0.0).equals(new AbstractMap.SimpleEntry<Map<Product, Double>, Map<Product, Double>>(acceptableProductsMeanEG1, unacceptableProductsMeanEG1)));
+	
+		Map<Product, Double> acceptableProductsMeanEG4 = new HashMap<Product, Double>();
+		acceptableProductsMeanEG4.put(testProduct6, Double.valueOf(0));
+		Map<Product, Double> unacceptableProductsMeanEG4 = new HashMap<Product, Double>();
+		assertTrue(eg4.getAcceptableAndUnacceptableProductsMean(0.0).equals(new AbstractMap.SimpleEntry<Map<Product, Double>, Map<Product, Double>>(acceptableProductsMeanEG4, unacceptableProductsMeanEG4)));
 	}
 
 }
